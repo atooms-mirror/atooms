1f102360b681852caf803c23005bf1aed3799224
Coslovich Daniele <daniele.coslovich@univ-montp2.fr>
2015-06-29 10:08:49 +0200

    Add hdf5 test

---
+++ tests/test_hdf5.py
@@ -0,0 +1,46 @@
+import unittest
+
+from atooms.system import System
+from atooms.system.cell import Cell
+from atooms.system.particle import Particle
+from atooms.trajectory import TrajectoryHDF5
+from atooms.interaction.interaction import Interaction
+from atooms.potential.potential import PairPotential
+from atooms.potential.cutoff import CutOff
+
+try:
+    import h5py
+    HAS_HDF5 = True
+except:
+    HAS_HDF5 = False
+
+class PairPotentialTest(unittest.TestCase):
+
+    def test_potential(self):
+        p = PairPotential("LennardJones", {"epsilon":1.0, "sigma":1.0}, [1,1], CutOff("CS", 2.5))
+
+    def test_interacting_system(self):
+        p = [PairPotential("LennardJones", {"epsilon":1.0, "sigma":1.0}, [1,1], CutOff("CS", 2.5))]
+        i = Interaction("atomic", p)
+        s = System()
+        s.interaction = i
+
+    @unittest.skipIf(not HAS_HDF5, 'no h5py module')
+    def test_write_initial_state(self):
+        p = [PairPotential("LennardJones", {"epsilon":1.0, "sigma":1.0}, [1,1], CutOff("CS", 2.5))]
+        i = [Interaction("atomic", p)]
+        s = System()
+        s.particle = [Particle(position=[1.0, 1.0, 1.0], velocity=[0.0, 0.0, 0.0])]
+        s.cell = Cell([1.0, 1.0, 1.0])
+        with TrajectoryHDF5('/tmp/test.h5', 'w') as t:
+            t.write_interaction(i)
+            t.write(s, 0)
+
+        with TrajectoryHDF5('/tmp/test.h5', 'r') as t:
+            i = t.read_interaction()
+            s = t[0]
+
+if __name__ == '__main__':
+    unittest.main()
+
+        
