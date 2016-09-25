e2033097004ff1818d0401a0349512842c92f1a1
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2016-09-25 22:25:17 +0200

    Add scaling of T and rho via decorator; we would be better off using callbacks instead

--- bin/trj.py
+++ bin/trj.py
@@ -17,7 +17,7 @@ trj_map = {
 
 def add_interaction_hdf5(finp, ff, tag=None):
     """Add interaction to hdf5 file"""
-
+    # TODO: is this a method in hdf5 or not?
     import h5py
 
     pid = os.getpid()
@@ -56,6 +56,35 @@ def add_interaction_hdf5(finp, ff, tag=None):
     for f in glob.glob(f_ref + '*'):
         os.remove(f)
 
+class Scaling(object):
+
+    def __new__(cls, component, scale, T):
+        cls = type('Scaling', (Scaling, component.__class__), component.__dict__)
+        return object.__new__(cls)
+
+    def __init__(self, component, factor=None, T=None):
+        self.component = component
+        self.factor = factor
+        self.T = T
+
+    def read_sample(self, *args, **kwargs):
+        s = super(Scaling, self).read_sample(*args, **kwargs)
+
+        if self.factor is not None:
+            s.cell.side *= self.factor
+            for p in s.particle:
+                p.position *= self.factor
+
+        if self.T is not None:
+            for p in s.particle:
+                p.maxwellian(self.T)
+            from atooms.system.particle import velocity_cm
+            v_cm = velocity_cm(s.particle)
+            for p in s.particle:
+                p.velocity -= v_cm
+
+        return s
+
 def main(t, args):
     if args.flatten_steps:
         t.steps = range(1,len(t)+1)
@@ -71,10 +100,19 @@ def main(t, args):
         # everything in ram (getitem doesnt provide a generator)
         ts = trajectory.Sliced(tn, sl)
 
-    if args.vel:
-        fout = trajectory.convert(ts, trj_map[args.out], tag=args.tag, stdout=args.stdout, include=['vx','vy','vz'])
+    # Change density and
+    if args.rho is not None or args.temperature is not None:
+        if args.rho is not None:
+            rho_0 = t[0].density
+            scale = (rho_0 / args.rho)**(1./3)
+        tt = Scaling(ts, scale, args.temperature)
     else:
-        fout = trajectory.convert(ts, trj_map[args.out], tag=args.tag, stdout=args.stdout)
+        tt = ts
+        
+    if args.vel:
+        fout = trajectory.convert(tt, trj_map[args.out], tag=args.tag, stdout=args.stdout, include=['vx','vy','vz'])
+    else:
+        fout = trajectory.convert(tt, trj_map[args.out], tag=args.tag, stdout=args.stdout)
 
     if args.ff:
         if os.path.exists(args.ff):
@@ -96,6 +134,8 @@ parser.add_argument('-F', '--ff',      dest='ff', type=str, default='', help='fo
 parser.add_argument('-g', '--gather',  dest='gather', action='store_true', help='gather files as a super trajectory in <dirname>-conv')
 parser.add_argument(      '--flatten-steps',dest='flatten_steps', action='store_true', help='use sample index instead of steps')
 parser.add_argument(      '--step',    dest='step', action='store', default=None, type=int, help='')
+parser.add_argument(      '--rho',     dest='rho', type=float, default=None, help='new density')
+parser.add_argument('-T', '--temperature',dest='temperature', type=float, default=None, help='new temperature')
 parser.add_argument(nargs='+',         dest='file',type=str, help='input files')
 args = parser.parse_args()
 
