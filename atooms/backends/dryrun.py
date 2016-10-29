dfeb8be2d756c72a40baf710ffea352c6f25a8a2
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2016-10-29 09:57:28 +0200

    Use System() as defined in backend.

--- atooms/backends/dryrun.py
+++ atooms/backends/dryrun.py
@@ -1,6 +1,42 @@
 # This file is part of atooms
 # Copyright 2010-2014, Daniele Coslovich
 
+class System(object):
+    
+    def potential_energy(self):
+        return 0.
+
+    def temperature(self):
+        return 0.
+
+    def mean_square_displacement(self, reference):
+        return 0.
+
+    @property
+    def thermostat(self):
+        pass
+
+
+class Trajectory(object):
+
+    suffix = 'dry'
+
+    def __init__(self, filename, mode='r'):
+        pass
+
+    def write(self, system, step):
+        pass
+
+    def close(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+    
+
 class DryRunBackend(object):
 
     def __init__(self, system=System()):
@@ -23,37 +59,3 @@ class DryRunBackend(object):
         pass
 
 
-class System(object):
-    
-    def potential_energy(self):
-        return 0.
-
-    def temperature(self):
-        return 0.
-
-    def mean_square_displacement(self, reference):
-        return 0.
-
-    @property
-    def thermostat(self):
-        pass
-
-class Trajectory(object):
-
-    suffix = 'dry'
-
-    def __init__(self, filename, mode='r'):
-        pass
-
-    def write(self, system, step):
-        pass
-
-    def close(self):
-        pass
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.close()
-    
