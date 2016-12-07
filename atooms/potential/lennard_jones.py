c0d946c77a78a013fd232bdd5700d221dd988fa2
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2016-12-07 15:17:15 +0100

    Tolerate eps in lj potential

--- atooms/potential/lennard_jones.py
+++ atooms/potential/lennard_jones.py
@@ -6,6 +6,8 @@ from potential import PairPotentialBase
 class LennardJones(PairPotentialBase):
 
     def _compute(self, rsq):
+        if 'eps' in self.params:
+            self.params['epsilon'] = self.params['eps']
         sigsq = self.params['sigma']**2
         u = 4 * self.params['epsilon'] * ((sigsq/rsq)**6 - (sigsq/rsq)**3)
         a = 24 * self.params['epsilon'] * (2*(sigsq/rsq)**6 - (sigsq/rsq)**3) / rsq
