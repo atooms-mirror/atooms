e93e02f5c8723e3606eff8afc43269ccc073aa04
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-02-10 13:50:55 +0100

    Add archived folder support

--- atooms/trajectory/folder.py
+++ atooms/trajectory/folder.py
@@ -7,43 +7,76 @@ This is a base class for trajectories that stores configurations as
 individual files in a directory. The `dirname` property stores the
 directory path; an additional `files` property stores the list of
 files sorted by step index.
+
+It supports compressed archives. In this case the input filename is
+the path to the archive, which will be decompressed in a temporary
+folder.
 """
 
 import os
 import glob
 import re
+import tarfile
+import tempfile
 
 from atooms.trajectory.base import TrajectoryBase
 
 
 class TrajectoryFolder(TrajectoryBase):
 
-    def __init__(self, dirname, mode='r', file_pattern='*', step_pattern='(\d*)'):
-        if not os.path.isdir(dirname):
-            raise IOError("Directory expected (%s)" % dirname)
-        TrajectoryBase.__init__(self, dirname, mode)
-        self.dirname = dirname
-        all_files = glob.glob(os.path.join(dirname, file_pattern))
-        file_steps = self._get_file_steps(all_files, step_pattern)
-        self.files = [a[0] for a in file_steps]
-        self.steps = [a[1] for a in file_steps]
+    def __init__(self, filename, mode='r', file_pattern='*', step_pattern='(\d*)'):
+        TrajectoryBase.__init__(self, filename, mode)
+        self.file_pattern = file_pattern
+        self.step_pattern = step_pattern
+        # See if trajectory is packed as a compressed tar file.
+        # If so, configurations will be extracted inplace and deleted at the end.
+        try:
+            dirname = tempfile.mkdtemp()
+            with tarfile.open(filename) as th:
+                th.extractall(path=dirname)
+                files = [os.path.join(dirname, f.name) for f in th.getmembers()]
+            self.dirname = dirname
+            self.archive = True
+        except:
+            if not os.path.isdir(filename):
+                raise IOError("Directory expected (%s)" % filename)
+            self.dirname = filename
+            self.archive = False
+            files = glob.glob(os.path.join(self.dirname, self.file_pattern))
 
-    def _get_file_steps(self, files, regexp):
+        self.files, self.steps = self._get_file_steps(files)
+
+    def _get_step(self, fileinp):
+        # Make sure we only test the basename (avoid metching
+        # patterns in directory path)
+        s = re.search(self.step_pattern, os.path.basename(fileinp))
+        if s:
+            step = int(s.group(1))
+            return step
+        else:
+            raise ValueError('Could not find step')          
+
+    def _get_file_steps(self, files):
         """Return a list of tuples (file, step). The step is extracted from
         the file path using `regexp`, which must contain one group for
         the step.
         """
         file_steps = []
         for i, f in enumerate(files):
-            # Make sure we only test the basename (avoid metching
-            # patterns in directory path)
-            fbase = os.path.basename(f)
-            s = re.search(regexp, fbase)
-            if s:
-                try:
-                    step = int(s.group(1))
-                except:
-                    step = i+1
-                file_steps.append((f, step))
+            if os.path.isdir(f):
+                continue
+            try:
+                step = self._get_step(f)
+            except ValueError:
+                step = i+1
+            file_steps.append((f, step))
         file_steps.sort(key = lambda a : a[1])
-        return file_steps
+        return [a[0] for a in file_steps], [a[1] for a in file_steps]
+
+    def close(self):
+        if self.archive:
+            os.rmdir(self.dirname)
+
+
+
+
