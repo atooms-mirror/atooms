c70d22292973e088d30d7124e3fb23e2319a6455
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-03-02 14:17:37 +0100

    Adapt md.py to new interface

--- bin/md.py
+++ bin/md.py
@@ -2,43 +2,39 @@
 
 """Simple molecular dynamics simulation driver using RUMD backend."""
 
-import sys
 import os
-from atooms.adapters.rumd_backend import Simulation, single
-from atooms.simulation import log
+from atooms.simulation import Simulation, Scheduler, WriterThermo, WriterConfig
+from atooms.backends.rumd_backend import single
+from atooms.utils import setup_logging
 
 def main(params):
     # TODO: dump params to a file in output_dir
     if params.verbose:
-        log.setLevel(10)
-    execfile(params.forcefield)
+        setup_logging(level=10)
+    with open(params.forcefield) as f:
+        exec(f.read())
     potential = locals()['_potential']
-    s = single(params.input_file,
-               potential,
-               params.T,
-               params.dt)
-    sa = Simulation(s, fixcm_interval=params.fixcm_interval,
-                     output_path=os.path.join(params.output_dir, 'configs'), 
-                     thermo_interval=params.thermo_interval,
-                     config_interval=params.config_interval,
+    s = single(params.input_file, potential, params.T, params.dt)
+    sa = Simulation(s, output_path=os.path.join(params.output_dir, 'trajectory'),
                      checkpoint_interval=params.config_interval,
                      steps=params.steps,
                      restart=params.restart)
+    sa.add(WriterThermo, Scheduler(params.thermo_interval))
+    sa.add(WriterConfig, Scheduler(params.config_interval))
     sa.run()
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', dest='verbose', action='store_true', help='verbose output')
-    parser.add_argument('-T', dest='T', type=float, help='temperature')
-    parser.add_argument('--dt', dest='dt', type=float, default=0.004, help='time step')
-    parser.add_argument('-t','--thermo-interval', dest='thermo_interval', type=int, default=50000, help='config interval (in units of swap periods)')
-    parser.add_argument('-c','--config-interval', dest='config_interval', type=int, default=50000, help='energy interval (in units of swap periods)')
-    parser.add_argument('--fixcm-interval', dest='fixcm_interval', type=int, default=100, help='interval in steps between CM fixing)')
-    parser.add_argument('-n','--steps', dest='steps', type=int, default=None, help='number of steps')
-    parser.add_argument('-i', dest='input_file', help='input_file')
+    parser.add_argument('-T',   dest='T', type=float, help='temperature')
+    parser.add_argument('--dt', dest='dt', type=float, default=0.002, help='time step')
+    parser.add_argument('-i',   dest='input_file', help='input_file')
     parser.add_argument('--ff', dest='forcefield', help='force field')
-    parser.add_argument('-r', dest='restart', action='store_true', help='restart')
+    parser.add_argument('-n',   dest='steps', type=int, default=0, help='number of steps')
+    parser.add_argument('-t','--thermo-interval', dest='thermo_interval', type=int, default=0, help='energy interval')
+    parser.add_argument('-c','--config-interval', dest='config_interval', type=int, default=0, help='config interval')
+    parser.add_argument('-r',   dest='restart', action='store_true', help='restart')
+    parser.add_argument('-v',   dest='verbose', action='store_true', help='verbose output')
     parser.add_argument(dest='output_dir',type=str, help='output directory')
     params = parser.parse_args()
 
