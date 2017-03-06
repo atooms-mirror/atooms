9d78dd9be0ebdbe16204f8ed8a60361078910902
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-03-06 22:26:31 +0100

    Fix test and update to new rumd backend interface

--- tests/test_rumd.py
+++ tests/test_rumd.py
@@ -4,7 +4,7 @@ import os
 import glob
 import unittest
 try:
-    from atooms.backends.rumd_backend import RumdBackend, single, multi
+    import rumd
     SKIP = False
 except ImportError:
     SKIP = True
@@ -13,26 +13,23 @@ from atooms.utils import setup_logging
 
 setup_logging(level=40)
 
-def potential():
-    import rumd
-    pot = rumd.Pot_LJ_12_6(cutoff_method = rumd.ShiftedPotential)
-    pot.SetParams(i=0, j=0, Epsilon=1.0, Sigma=1.0, Rcut=2.5)
-    pot.SetParams(i=1, j=0, Epsilon=1.5, Sigma=0.8, Rcut=2.5)
-    pot.SetParams(i=0, j=1, Epsilon=1.5, Sigma=0.8, Rcut=2.5)
-    pot.SetParams(i=1, j=1, Epsilon=0.5, Sigma=0.88, Rcut=2.5)
-    pot.SetVerbose(False)
-    return [pot]
-
 class Test(unittest.TestCase):
 
     def setUp(self):
         if SKIP:
             self.skipTest('missing RUMD')
         self.input_file = os.path.join(os.path.dirname(__file__), '../data/KA_N256_rumd.xyz.gz')
-            
+        self.forcefield_file = os.path.join(os.path.dirname(__file__), '../data/ka_rumd.ff')            
+        from atooms.backends.rumd_backend import RumdBackend
+        self.backend = RumdBackend(self.input_file,
+                                   self.forcefield_file, integrator='nvt',
+                                   temperature=0.80, dt=0.002)
+        self.backend_2 = RumdBackend(self.input_file,
+                                     self.forcefield_file, integrator='nvt',
+                                     temperature=0.80, dt=0.002)
+
     def test_single(self):
-        s = single(self.input_file, potential, T=0.80, dt=0.002)
-        si = Simulation(RumdBackend(s),
+        si = Simulation(self.backend,
                         output_path='/tmp/test_rumd_single/trajectory',
                         steps=2000, checkpoint_interval=100,
                         restart=False)
@@ -43,8 +40,7 @@ class Test(unittest.TestCase):
         self.assertEqual(len(ls), 21)
 
     def test_multi(self):
-        s = single(self.input_file, potential, T=0.80, dt=0.002)
-        si = Simulation(RumdBackend(s),
+        si = Simulation(self.backend,
                         output_path='/tmp/test_rumd_multi/trajectory',
                         steps=2000, checkpoint_interval=100,
                         restart=False)
@@ -54,8 +50,7 @@ class Test(unittest.TestCase):
         si.run(1000)
 
     def test_multi_writing(self):
-        s = single(self.input_file, potential, T=0.80, dt=0.002)
-        si = Simulation(RumdBackend(s),
+        si = Simulation(self.backend,
                         output_path='/tmp/test_rumd_multi_writing/trajectory',
                         enable_speedometer=False)
         si.add(WriterConfig(), Scheduler(500))
@@ -65,16 +60,14 @@ class Test(unittest.TestCase):
         self.assertEqual(len(ls), 11)
 
     def test_multi_2(self):
-        s = single(self.input_file, potential, T=0.80, dt=0.002)
-        si = Simulation(RumdBackend(s),
+        si = Simulation(self.backend,
                         output_path='/tmp/test_rumd_multi_2/trajectory',
                         steps=2000, checkpoint_interval=100,
                         restart=False)
         si.add(WriterThermo(), Scheduler(100))
         si.add(WriterConfig(), Scheduler(100))
         si.run()
-        s = single(s, potential, T=0.80, dt=0.002)
-        si = Simulation(RumdBackend(s),
+        si = Simulation(self.backend_2,
                         output_path='/tmp/test_rumd_multi_2/trajectory',
                         steps=1000, checkpoint_interval=100,
                         restart=False)
@@ -83,8 +76,7 @@ class Test(unittest.TestCase):
         si.run()
 
     def test_multi_rmsd(self):
-        s = single(self.input_file, potential, T=0.80, dt=0.002)
-        si = Simulation(RumdBackend(s),
+        si = Simulation(self.backend,
                         output_path='/tmp/test_rumd_multi_rmsd/trajectory',
                         checkpoint_interval=100, steps=1000000000,
                         restart=False)
