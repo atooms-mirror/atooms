6733b88e26cdb6e98097bd381b06ee409c7bab7b
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-03-12 13:44:42 +0100

    Move custom testcase to statunittest

--- atooms/utils/__init__.py
+++ atooms/utils/__init__.py
@@ -233,20 +233,3 @@ def __header_dict(line):
     for key, value in [d.split('=') for d in line.split()]:
         params[key] = value
     return params
-
-# Unit test enhancements
-
-# unittest in python 2.7 has a convenient argument delta in
-# assertAlmostEqual. However, this is missing in python 2.6. To avoid
-# requiring unittest2, which backports new features introduced in 2.7,
-# we provide our custom subclass of TestCase.
-
-import unittest
-
-class TestCase(unittest.TestCase):
-    
-    def assertAlmostEqual(self, first, second, places=7, msg=None, delta=None):
-        if delta is None:
-            unittest.TestCase.assertAlmostEqual(self, first, second, places, msg)
-        else:
-            return abs(first-second) <= delta
