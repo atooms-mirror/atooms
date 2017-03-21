e49fe9dea2f27df94f0d63bf3e8e6356a670c82f
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-03-22 00:12:10 +0100

    Update write thermo interface

--- atooms/simulation/observers.py
+++ atooms/simulation/observers.py
@@ -109,7 +109,7 @@ class WriterThermo(object):
         f = sim.base_path + '.thermo'
         with open(f, 'a') as fh:
             fh.write('%d %g %g\n' % (sim.steps,
-                                     sim.system.potential_energy(normed=True),
+                                     sim.system.potential_energy(),
                                      sim.rmsd()))
 
     def clear(self, sim):
