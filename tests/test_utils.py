d2a2412b2a85aa144804dce15c325dc700ba3784
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-04-13 17:02:18 +0200

    Add test

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import unittest
+from atooms.trajectory import utils
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_block_period(self):
+        tpl = [0, 1, 2, 4, 8, 16, 36]
+        more = tpl + [1+tpl[-1]+i for i in tpl]
+        self.assertEqual(utils.get_period(more), 7)
+        self.assertEqual(utils.check_block_period(more, utils.get_period(more)), None)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=0)
+
+
