8f80cee84090d811a8a93bf399ac5879ffc45f55
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-09-16 00:02:30 +0200

    Drop dead files

--- tests/fuzzyunittest.py
+++
@@ -1,9 +0,0 @@
-from unittest import *
-
-class FuzzyTestCase(TestCase):
-
-    def assertAlmostEqual(x, y, reltol=1e-3):
-        """Return True if x and y are equal to provided relative tolerance"""
-        delta = abs(x-y)
-        return self.assertLess(delta, reltol * 0.5*(x+y))
-
