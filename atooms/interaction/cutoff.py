caeeb0b8275b3a62bd8929429b328a6f02d6c2c4
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2018-01-13 09:01:08 +0100

    Add cspl cut off and improve tabulation

--- atooms/interaction/cutoff.py
+++ atooms/interaction/cutoff.py
@@ -20,6 +20,8 @@ class CutOff(object):
         self.scheme = scheme
         self.radius = radius
         self.rcutsq = radius**2
+        self.radius_mid = radius
+        self.radius_mid_sq = radius**2
 
     def __str__(self):
         return self.scheme
@@ -43,6 +45,21 @@ class CutOff(object):
             self.vcut = u[0]
         elif self.scheme in ['c', 'cut']:
             pass
+        elif self.scheme in ['cspl', 'CSPL']:
+            # the potential is cubic-splined between rcut1 < r < rcut 
+            # cubic-splined cutoff : u(r) = A*(B-r)^3 + C for rcut1 < r < rcut=B 
+            # where 
+            #   A = - u2^2/(12*u1)
+            #   B = rcut1 - 2*(u1/u2) = rcut
+            #   C = u0 + (2*u1^2)/(3*u2)
+            # with u0=u(rcut1), u1=u'(rcut1), u2=u''(rcut1)
+            u2 = u[2]*self.radius_mid**2 - u[1]  # second order derivative
+            u1 = - u[1] * self.radius_mid  # first order derivative
+            u0 = u[0]
+            self.Acut = - u2**2 / (12*u1)
+            self.Ccut = - u0 + (2*u1**2) / (3*u2)
+            self.radius = self.radius_mid - (2*u1/u2)
+            self.radius_sq = self.radius**2 
         else:
             raise NotImplementedError()
 
@@ -58,6 +75,16 @@ class CutOff(object):
             u_new[0] = u[0] - self.vcut
         elif self.scheme in ['c', 'cut']:
             pass
+        elif self.scheme in ['cspl', 'CSPL']:
+            # cubic splined between rcut1 and rcut :
+            # function is overwritten when between rcut1 and rcut
+            if rsquare > self.radius_mid_sq:
+                rij = rsquare**0.5
+                dr  = self.radius - rij
+                u_new[0] = self.Acut * dr**3 
+                u_new[1] = 3 * self.Acut * dr**2 / rij
+            else:
+                u_new[0] = u[0] + self.Ccut
         else:
             raise NotImplementedError()
         return u_new
