a58384137ef6e2151bce77d4f1faf070ea1021ed
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2018-12-23 19:08:54 +0100

    Fix wall / clock times

--- atooms/core/utils.py
+++ atooms/core/utils.py
@@ -147,6 +147,10 @@ class Timer(object):
         self.__start_wall = None
         self.cpu_time = 0.0
         self.wall_time = 0.0
+        try:
+            self._wall_time_func = MPI.Wtime
+        except:
+            self._wall_time_func = time.time
 
     def start(self):
         self.__start_cpu = self.__now_cpu()
@@ -159,13 +163,10 @@ class Timer(object):
         self.wall_time += self.__now_wall() - self.__start_wall
 
     def __now_cpu(self):
-        return time.time()
+        return time.clock()
 
     def __now_wall(self):
-        try:
-            return MPI.Wtime()
-        except:
-            return time.clock()
+        return self._wall_time_func()
 
 
 def clockit(func):
@@ -284,11 +285,6 @@ def setup_logging(name=None, level=40, filename=None, update=False):
     else:
         log.addHandler(handler)
     
-    # Activate progress bar support by default when level is 20 (but not 10)
-    if log.getEffectiveLevel() == 20:
-        import atooms.core.progress
-        atooms.core.progress.active = True
-
     return log
 
 
