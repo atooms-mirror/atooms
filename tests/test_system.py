8c5b9d9864771dce43b1acd35fe8b0f56e719086
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2019-05-22 21:17:12 +0200

    Fix dump() behavior

--- tests/test_system.py
+++ tests/test_system.py
@@ -253,6 +253,31 @@ class Test(unittest.TestCase):
         self.assertFalse((p[0].position == pos[:, 0]).all())
         self.assertFalse(numpy.may_share_memory(p[0].position, pos[:, 0]))
 
+    def test_dump_cbk(self):
+        """
+        Make sure that applying callbacks or changing the particle array
+        size creates a new dump.
+        """
+        import numpy
+        from atooms.system import Particle, System
+
+        p = [Particle(), Particle()]
+        s = System(p)
+        pos1 = s.dump("pos")
+        def cbk(system):
+            s = copy.copy(system)
+            s.particle = [system.particle[0]]
+            return s
+        s = cbk(s)
+        pos2 = s.dump('pos')
+        self.assertEqual(pos1.shape, (2, 3))
+        self.assertEqual(pos2.shape, (1, 3))
+        # Grandcanonical
+        s.particle.append(Particle())
+        self.assertEqual(s.dump('pos').shape, (2, 3))
+        # Change Particle
+        s.particle[0] = Particle(position=[1.0, 1.0, 1.0])
+        self.assertEqual(s.dump('pos')[0][0], 1.0)
 
 if __name__ == '__main__':
     unittest.main()
