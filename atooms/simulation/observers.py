f44554720eaa2391d8a667186bf01c23bcc3005a
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2020-01-16 13:45:26 +0100

    Fix handling of restart in write_thermo() and write_config() observers

--- atooms/simulation/observers.py
+++ atooms/simulation/observers.py
@@ -171,11 +171,14 @@ def write_config(sim, fields=None, precision=None):
     instance.
     """
     # Initialize
-    if sim.current_step == 0 and hasattr(sim, '__init_write_config'):
-        del(sim.__init_write_config)
+    # This will clear the variable in a new run
+    if sim.current_step == 0 or not hasattr(sim, '__init_write_config'):
+        sim.__init_write_config = False
+    if sim.restart:
+        sim.__init_write_config = True
 
     # Header
-    if not hasattr(sim, '__init_write_config'):
+    if not sim.__init_write_config:
         sim.__init_write_config = True
         # TODO: folder-based trajectories should ensure that mode='w' clears up the folder
         rmd(sim.output_path)
@@ -201,7 +204,7 @@ def write_thermo(sim, fields=None, fmt=None, precision=6, functions=None):
     - total energy
     - pressure
     - rmsd
-
+    
     The set of available `fields` can be augmented by passing an extra
     `functions` dictionary.
 
@@ -250,12 +253,16 @@ def write_thermo(sim, fields=None, fmt=None, precision=6, functions=None):
     if fmt is not None:
         _db_fmt.update(fmt)
 
+    # TODO: make it possible to have multiple write_thermo callbacks. At present they interfere
     # Initialize
-    if sim.current_step == 0 and hasattr(sim, '__init_write_thermo'):
-        del(sim.__init_write_thermo)
+    # This will clear the variable in a new run
+    if sim.current_step == 0 or not hasattr(sim, '__init_write_thermo'):
+        sim.__init_write_thermo = False
+    if sim.restart:
+        sim.__init_write_thermo = True
 
     # Header
-    if not hasattr(sim, '__init_write_thermo'):
+    if not sim.__init_write_thermo:
         sim.__init_write_thermo = True
         with open(sim.output_path + '.thermo', 'w') as fh:
             txt = ', '.join(fields)
@@ -395,7 +402,7 @@ def shell_stop(sim, cmd, exit_code=1):
         if len(output) > 0:
             _log.info('shell command "{}" returned: {}'.format(cmd, output.strip()))
 
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError as e: 
         # We stop the simulation
         if e.returncode == exit_code:
             raise SimulationEnd('shell command "{}" returned "{}"'.format(cmd, e.output.strip()))
@@ -464,7 +471,7 @@ class Speedometer(object):
             d_now = datetime.datetime.now()
             d_delta = datetime.timedelta(seconds=eta)
             d_eta = d_now + d_delta
-            # self._callback.__name__,
+            # self._callback.__name__, 
             _log.info('%2d%% ETA: %s S/T: %.1f T/SP: %.2e',
                       int(frac * 100),
                       d_eta.strftime('%Y-%m-%d %H.%M'),
