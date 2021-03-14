9981e37bb2f632f851537f8f5aac6cda7f37d290
Daniele Coslovich <92140-coslo@users.noreply.framagit.org>
2021-03-14 10:42:16 +0100

    Test

--- tests/test_f90.py
+++ tests/test_f90.py
@@ -25,59 +25,59 @@ class Test(unittest.TestCase):
         self.data = os.path.join(os.path.dirname(__file__),'../data/')
         self.trajectory = f90.Trajectory(os.path.join(self.data, 'lj_N256_rho1.0.xyz'))
 
-    @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
-    def test_collinear(self):
-        import atooms.models
-        from atooms.backends.f90 import Interaction, System, Particle, Cell
-        db = atooms.models.load()
-        model = db["lennard_jones"]
-        particles = [Particle(position=[0.0, 0.0, 0.0], species=1),
-                     Particle(position=[1.0, 0.0, 0.0], species=1),
-                     Particle(position=[2.0, 0.0, 0.0], species=1)]
-        cell = Cell([10., 10., 10.])
-        system = System(particles, cell)
-        system.interaction = Interaction(model)
-        self.assertAlmostEqual(system.potential_energy(), -0.01257276409199999)
+    # @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
+    # def test_collinear(self):
+    #     import atooms.models
+    #     from atooms.backends.f90 import Interaction, System, Particle, Cell
+    #     db = atooms.models.load()
+    #     model = db["lennard_jones"]
+    #     particles = [Particle(position=[0.0, 0.0, 0.0], species=1),
+    #                  Particle(position=[1.0, 0.0, 0.0], species=1),
+    #                  Particle(position=[2.0, 0.0, 0.0], species=1)]
+    #     cell = Cell([10., 10., 10.])
+    #     system = System(particles, cell)
+    #     system.interaction = Interaction(model)
+    #     self.assertAlmostEqual(system.potential_energy(), -0.01257276409199999)
 
-    def test_trajectory(self):
-        # Make sure the original trajectories have no wrappers
-        with Trajectory(os.path.join(self.data, 'lj_N256_rho1.0.xyz')) as trajectory:
-            self.assertEqual(trajectory.class_callbacks, None)
+    # def test_trajectory(self):
+    #     # Make sure the original trajectories have no wrappers
+    #     with Trajectory(os.path.join(self.data, 'lj_N256_rho1.0.xyz')) as trajectory:
+    #         self.assertEqual(trajectory.class_callbacks, None)
 
-    @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
-    def test_energy(self):
-        system = self.trajectory[0]
-        system.compute_interaction()
-        self.assertAlmostEqual(system.potential_energy(per_particle=True, cache=True), -3.8079776291909284)
+    # @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
+    # def test_energy(self):
+    #     system = self.trajectory[0]
+    #     system.compute_interaction()
+    #     self.assertAlmostEqual(system.potential_energy(per_particle=True, cache=True), -3.8079776291909284)
 
-    def test_neighbors(self):
-        rcut = [[1.5]]
-        system = self.trajectory[0]
-        system.neighbor_list = f90.NeighborList(rcut)
-        system._compute_neighbor_list()
-        self.assertEqual(list(system.particle[0].neighbors), [17, 32, 52, 91, 109, 112, 121, 140, 149, 162, 181, 198, 231, 247, 256])
+    # def test_neighbors(self):
+    #     rcut = [[1.5]]
+    #     system = self.trajectory[0]
+    #     system.neighbor_list = f90.NeighborList(rcut)
+    #     system._compute_neighbor_list()
+    #     self.assertEqual(list(system.particle[0].neighbors), [17, 32, 52, 91, 109, 112, 121, 140, 149, 162, 181, 198, 231, 247, 256])
 
-    @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
-    def test_model(self):
-        with f90.Trajectory(os.path.join(self.data, 'lj_rho0.85.xyz')) as trajectory:
-            trajectory.metadata['model'] = 'lennard_jones'
-            system = trajectory[0]
-            self.assertAlmostEqual(system.potential_energy(per_particle=True), -2.24379330538)
+    # @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
+    # def test_model(self):
+    #     with f90.Trajectory(os.path.join(self.data, 'lj_rho0.85.xyz')) as trajectory:
+    #         trajectory.metadata['model'] = 'lennard_jones'
+    #         system = trajectory[0]
+    #         self.assertAlmostEqual(system.potential_energy(per_particle=True), -2.24379330538)
 
-    @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
-    def test_interface(self):
-        from atooms.backends.f90 import Interaction
-        interaction = Interaction("lennard_jones")
+    # @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
+    # def test_interface(self):
+    #     from atooms.backends.f90 import Interaction
+    #     interaction = Interaction("lennard_jones")
             
-    @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
-    def test_model(self):
-        with f90.Trajectory(os.path.join(self.data, 'lj_rho0.85.xyz')) as trajectory:
-            trajectory.metadata['model'] = 'lennard_jones'
-            system = trajectory[0]
-            self.assertAlmostEqual(system.potential_energy(per_particle=True), -2.24379330538)
+    # @unittest.skipIf(not HAS_MODELS, 'no atooms-models module')
+    # def test_model(self):
+    #     with f90.Trajectory(os.path.join(self.data, 'lj_rho0.85.xyz')) as trajectory:
+    #         trajectory.metadata['model'] = 'lennard_jones'
+    #         system = trajectory[0]
+    #         self.assertAlmostEqual(system.potential_energy(per_particle=True), -2.24379330538)
         
-    def tearDown(self):
-        self.trajectory.close()
+    # def tearDown(self):
+    #     self.trajectory.close()
 
 if __name__ == '__main__':
     unittest.main()
