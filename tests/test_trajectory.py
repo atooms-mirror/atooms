b1772c131351954d12f729e4c554d3abaadef6f4
Daniele Coslovich <92140-coslo@users.noreply.framagit.org>
2021-03-16 14:28:29 +0100

    Move lammps trajectory tests

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -399,6 +399,85 @@ B 2.9 -2.9 0.0
                 'grandcanonical']
             info(th, keys=','.join(keys))
             
+    def test_lammps(self):
+        import sys
+        with open(self.inpfile, 'w') as fh:
+            fh.write("""\
+ITEM: TIMESTEP
+0
+ITEM: NUMBER OF ATOMS
+2
+ITEM: BOX BOUNDS pp pp pp
+-3 3
+-3 3
+-3 3
+ITEM: ATOMS id type xs ys zs
+2 1 0.10 0.11 0.12
+1 1 0.20 0.21 0.22
+ITEM: TIMESTEP
+1
+ITEM: NUMBER OF ATOMS
+2
+ITEM: BOX BOUNDS pp pp pp
+-4 4
+-4 4
+-4 4
+ITEM: ATOMS id type xs ys zs
+1 1 0.00 0.01 0.02
+2 1 0.50 0.51 0.52
+""")
+        from atooms.trajectory import TrajectoryLAMMPS
+        def scale(pos, side):
+            return [(x - 0.5) * L for x, L in zip(pos, side)]
+        with TrajectoryLAMMPS(self.inpfile) as th:
+            self.assertEqual(list(th[0].cell.side), [6.0, 6.0, 6.0])
+            self.assertEqual(list(th[0].particle[0].position), scale([0.20, 0.21, 0.22], [6.0, 6.0, 6.0]))
+            self.assertEqual(list(th[0].particle[1].position), scale([0.10, 0.11, 0.12], [6.0, 6.0, 6.0]))
+            self.assertEqual(list(th[1].cell.side), [8.0, 8.0, 8.0])
+            self.assertEqual(list(th[1].particle[0].position), scale([0.00, 0.01, 0.02], [8.0, 8.0, 8.0]))
+            self.assertEqual(list(th[1].particle[1].position), scale([0.50, 0.51, 0.52], [8.0, 8.0, 8.0]))
+
+    def test_lammps_folder(self):
+        with open(self.inpdir + '/0.atom', 'w') as fh:
+            fh.write("""\
+ITEM: TIMESTEP
+10
+ITEM: NUMBER OF ATOMS
+2
+ITEM: BOX BOUNDS pp pp pp
+-3 3
+-3 3
+-3 3
+ITEM: ATOMS id type xs ys zs
+2 1 0.10 0.11 0.12
+1 1 0.20 0.21 0.22
+""")
+        with open(self.inpdir + '/1.atom', 'w') as fh:
+            fh.write("""\
+ITEM: TIMESTEP
+20
+ITEM: NUMBER OF ATOMS
+2
+ITEM: BOX BOUNDS pp pp pp
+-4 4
+-4 4
+-4 4
+ITEM: ATOMS id type xs ys zs
+1 1 0.00 0.01 0.02
+2 1 0.50 0.51 0.52
+""")
+        from atooms.trajectory import TrajectoryFolderLAMMPS
+        def scale(pos, side):
+            return [(x - 0.5) * L for x, L in zip(pos, side)]
+        with TrajectoryFolderLAMMPS(self.inpdir) as th:
+            self.assertEqual(th.steps, [10, 20])
+            self.assertEqual(list(th[0].cell.side), [6.0, 6.0, 6.0])
+            self.assertEqual(list(th[0].particle[0].position), scale([0.20, 0.21, 0.22], [6.0, 6.0, 6.0]))
+            self.assertEqual(list(th[0].particle[1].position), scale([0.10, 0.11, 0.12], [6.0, 6.0, 6.0]))
+            self.assertEqual(list(th[1].cell.side), [8.0, 8.0, 8.0])
+            self.assertEqual(list(th[1].particle[0].position), scale([0.00, 0.01, 0.02], [8.0, 8.0, 8.0]))
+            self.assertEqual(list(th[1].particle[1].position), scale([0.50, 0.51, 0.52], [8.0, 8.0, 8.0]))
+
     def tearDown(self):
         rmf(self.inpfile)
         rmd(self.inpdir)
