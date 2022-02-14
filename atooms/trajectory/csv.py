e3144faf1c5bb4897b97ddcda775342e12608d9d
Daniele Coslovich <92140-coslo@users.noreply.framagit.org>
2022-02-14 14:28:41 +0100

    Fix TrajectoryCSV

--- atooms/trajectory/csv.py
+++ atooms/trajectory/csv.py
@@ -10,7 +10,8 @@ class TrajectoryCSV(TrajectoryBase):
         self._file = open(self.filename, self.mode)
         self.precision = 4
         self._ndim = 3
-        self._sep = ' '
+        self._sep = None
+        self._order = 'C'
         if mode == 'r':
             self._setup()
     
@@ -29,12 +30,14 @@ class TrajectoryCSV(TrajectoryBase):
 
     def read_system(self, frame):
         self._file.seek(self._file_index[frame])
-        data = self._file.readline()
+        data = self._file.readline().strip()
         pos = numpy.array([float(_) for _ in data.split(self._sep)])        
         npart = len(pos) // self._ndim
+        pos = pos.reshape((npart, self._ndim))
         system = System()
         for i in range(npart):
-            p = Particle(position=pos[i*self._ndim: (i+1)*self._ndim])
+            #p = Particle(position=pos[i*self._ndim: (i+1)*self._ndim])
+            p = Particle(position=pos[i, :])
             p.position_unfolded = p.position
             system.particle.append(p)
         return system
@@ -43,7 +46,7 @@ class TrajectoryCSV(TrajectoryBase):
         sep = ' '
         fmt = '{:.' + str(self.precision) + 'g}'
         numpy.set_string_function(lambda x: sep.join([fmt.format(xi) for xi in x]), False)
-        self._file.write(str(system.dump('position', flat=True)) + '\n')
+        self._file.write(str(system.dump('position', flat=True, order=self._order)) + '\n')
         numpy.set_string_function(None, False)
 
 
